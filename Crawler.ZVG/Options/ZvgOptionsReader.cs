09506025b2a80fa053d64f3c427e0d39b3e4ddc3
Niklas <niklas@nokxs.de>
2020-07-21 22:04:50 +0200

    Add Persistence

--- Crawler.ZVG/Options/ZvgOptionsReader.cs
+++ Crawler.ZVG/Options/ZvgOptionsReader.cs
@@ -7,9 +7,15 @@ namespace Crawler.Provider.ZVG.Options
     {
         public ZvgWebClientOptions GetWebClientOptions()
         {
+            //https://www.zvg.com/appl/termine.prg?act=getGridData&vt=&id_b=4&ids=159,114,49,86,105,&ido=35,4,5,49,&sort=a
+
             var stateId = Environment.GetEnvironmentVariable("PROVIDER_ZVG_STATE_ID");
             var courtIds = Environment.GetEnvironmentVariable("PROVIDER_ZVG_COURT_IDS");
             var objectKindIds = Environment.GetEnvironmentVariable("PROVIDER_ZVG_OBJECT_KIND_ID");
+
+            stateId = "4";
+            courtIds = "35,4,5,49";
+            objectKindIds = "159,114,49,86,105";
 
             return new ZvgWebClientOptions(stateId, courtIds, objectKindIds);
         }
