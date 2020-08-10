ded7a397d98301c46bd3d3f834c61b7d9a656fb7
Niklas <niklas@nokxs.de>
2020-08-10 13:22:01 +0200

    Configure zvg via environment variables

---
+++ PropertyBot.ZVG/EnvironmentConstants.cs
@@ -0,0 +1,11 @@
+﻿namespace PropertyBot.Provider.ZVG
+{
+    public static class EnvironmentConstants
+    {
+        public static string PROVIDER_ZVG_STATE_ID = "PROVIDER_ZVG_STATE_ID";
+
+        public static string PROVIDER_ZVG_COURT_IDS = "PROVIDER_ZVG_COURT_IDS";
+
+        public static string PROVIDER_ZVG_OBJECT_KIND_ID = "PROVIDER_ZVG_OBJECT_KIND_ID";
+    }
+}
--- PropertyBot.ZVG/Options/ZvgOptionsReader.cs
+++ PropertyBot.ZVG/Options/ZvgOptionsReader.cs
@@ -9,14 +9,10 @@ namespace PropertyBot.Provider.ZVG.Options
         {
             //https://www.zvg.com/appl/termine.prg?act=getGridData&vt=&id_b=4&ids=159,114,49,86,105,&ido=35,4,5,49,&sort=a
 
-            var stateId = Environment.GetEnvironmentVariable("PROVIDER_ZVG_STATE_ID");
-            var courtIds = Environment.GetEnvironmentVariable("PROVIDER_ZVG_COURT_IDS");
-            var objectKindIds = Environment.GetEnvironmentVariable("PROVIDER_ZVG_OBJECT_KIND_ID");
-
-            stateId = "4";
-            courtIds = "35,4,5,49";
-            objectKindIds = "159,114,49,86,105";
-
+            var stateId = Environment.GetEnvironmentVariable(EnvironmentConstants.PROVIDER_ZVG_STATE_ID);
+            var courtIds = Environment.GetEnvironmentVariable(EnvironmentConstants.PROVIDER_ZVG_COURT_IDS);
+            var objectKindIds = Environment.GetEnvironmentVariable(EnvironmentConstants.PROVIDER_ZVG_OBJECT_KIND_ID);
+            
             return new ZvgWebClientOptions(stateId, courtIds, objectKindIds);
         }
     }
