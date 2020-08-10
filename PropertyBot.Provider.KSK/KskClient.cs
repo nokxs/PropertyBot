983fbdff3e983c4b7798b573a541f584f2d52b90
Niklas <niklas@nokxs.de>
2020-08-10 15:31:21 +0200

    Fix KSK client location

---
+++ PropertyBot.Provider.KSK/KskClient.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+using PropertyBot.Interface;
+
+namespace PropertyBot.Provider.KSK
+{
+    public class KskClient : IPropertyProvider
+    {
+        public Task<IEnumerable<Property>> GetProperties()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
