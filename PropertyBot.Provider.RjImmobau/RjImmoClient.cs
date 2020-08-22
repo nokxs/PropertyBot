96d89343409e96f2c0571ccb1ec77881696ddd3a
Niklas <niklas@nokxs.de>
2020-08-22 15:14:48 +0200

    Support RJ Immobau

---
+++ PropertyBot.Provider.RjImmobau/RjImmoClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PropertyBot.Common;
+using PropertyBot.Interface;
+using PropertyBot.Provider.Base.ImmoXXL;
+using PropertyBot.Provider.Base.ImmoXXL.WebClient;
+
+namespace PropertyBot.Provider.RjImmobau
+{
+    public class RjImmoClient : IPropertyProvider
+    {
+        private readonly IImmoXXLClient _immoXxlClient;
+
+        public RjImmoClient(IImmoXXLClient immoXxlClient)
+        {
+            _immoXxlClient = immoXxlClient;
+        }
+
+        public async Task<IEnumerable<Property>> GetProperties()
+        {
+            return await _immoXxlClient.GetProperties(GetWebClientOptions());
+        }
+        
+        private ImmoXXLWebClientOptions GetWebClientOptions()
+        {
+            var buyIds = EnvironmentConstants.PROVIDER_RJIMMO_IMMO_BUY_IDS.GetAsOptionalEnvironmentVariable("1");
+            var categoryIds = EnvironmentConstants.PROVIDER_RJIMMO_IMMO_CATEGORY_IDS.GetAsOptionalEnvironmentVariable("200");
+
+            return new ImmoXXLWebClientOptions("http://www.rjimmobau.de", buyIds, categoryIds);
+        }
+    }
+}
---
+++ PropertyBot.Provider.RjImmobau/RjImmoProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿using PropertyBot.Interface;
+using PropertyBot.Provider.Base.ImmoXXL;
+
+namespace PropertyBot.Provider.RjImmobau
+{
+    public class RjImmoProviderFactory
+    {
+        public static IPropertyProvider CreateProvider()
+        {
+            var immoXxlClient = ImmoXXLProviderFactory.CreateClient();
+
+            return new RjImmoClient(immoXxlClient);
+        }
+    }
+}
