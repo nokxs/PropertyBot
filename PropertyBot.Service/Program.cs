364302bd7445b3c66931c854b70a1cf9bd4d261a
Niklas <niklas@nokxs.de>
2020-08-22 11:52:18 +0200

    Activate all providers

--- PropertyBot.Service/Program.cs
+++ PropertyBot.Service/Program.cs
@@ -39,10 +39,10 @@ namespace PropertyBot.Service
 
         private static void RegisterPropertyProviders(IServiceCollection services)
         {
-            //services.AddSingleton(ZvgProviderFactory.CreateProvider());
-            //services.AddSingleton(KskProviderFactory.CreateProvider());
-            //services.AddSingleton(VolksbankImmopoolProviderFactory.CreateProvider());
-            //services.AddSingleton(LinkImmoProviderFactory.CreateProvider());
+            services.AddSingleton(ZvgProviderFactory.CreateProvider());
+            services.AddSingleton(KskProviderFactory.CreateProvider());
+            services.AddSingleton(VolksbankImmopoolProviderFactory.CreateProvider());
+            services.AddSingleton(LinkImmoProviderFactory.CreateProvider());
             services.AddSingleton(GutImmoProviderFactory.CreateProvider());
         }
 
