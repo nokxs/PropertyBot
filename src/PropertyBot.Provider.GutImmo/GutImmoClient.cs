5547239841ba50ab3b8a675ebc039a20bab9cf4f
Niklas <niklas@nokxs.de>
2020-09-19 20:43:27 +0200

    Re-Add gut immo

---
+++ src/PropertyBot.Provider.GutImmo/GutImmoClient.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using PropertyBot.Common;
+using PropertyBot.Interface;
+using PropertyBot.Provider.Base.ImmoXXL;
+using PropertyBot.Provider.Base.ImmoXXL.WebClient;
+
+namespace PropertyBot.Provider.GutImmo
+{
+    public class GutImmoClient : IPropertyProvider
+    {
+        private readonly IImmoXXLClient _immoXxlClient;
+
+        internal GutImmoClient(IImmoXXLClient immoXxlClient)
+        {
+            _immoXxlClient = immoXxlClient;
+        }
+
+        public async Task<IEnumerable<Property>> GetProperties()
+        {
+            return await _immoXxlClient.GetProperties(GetWebClientOptions());
+        }
+
+        private ImmoXXLWebClientOptions GetWebClientOptions()
+        {
+            var buyIds = EnvironmentConstants.PROVIDER_GUT_IMMO_BUY_IDS.GetAsOptionalEnvironmentVariable("1");
+            var categoryIds = EnvironmentConstants.PROVIDER_GUT_IMMO_CATEGORY_IDS.GetAsOptionalEnvironmentVariable("200");
+
+            return new ImmoXXLWebClientOptions("https://www.gutimmo.de", buyIds, categoryIds);
+        }
+    }
+}
---
+++ src/PropertyBot.Provider.GutImmo/GutImmoProviderFactory.cs
@@ -0,0 +1,15 @@
+﻿using PropertyBot.Interface;
+using PropertyBot.Provider.Base.ImmoXXL;
+
+namespace PropertyBot.Provider.GutImmo
+{
+    public class GutImmoProviderFactory
+    {
+        public static IPropertyProvider CreateProvider()
+        {
+            var immoXxlClient = ImmoXXLProviderFactory.CreateClient("Gut Immo");
+
+            return new GutImmoClient(immoXxlClient);
+        }
+    }
+}
