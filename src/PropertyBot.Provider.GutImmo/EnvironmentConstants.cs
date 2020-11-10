161d16a4dcfa4dc23d604f14adc9c15cdb1e5bbd
Niklas <niklas@nokxs.de>
2020-11-10 21:26:29 +0100

    Remove unused projects

--- src/PropertyBot.Provider.GutImmo/EnvironmentConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PropertyBot.Provider.GutImmo
-{
-    internal static class EnvironmentConstants
-    {
-        public static string PROVIDER_GUT_IMMO_BUY_IDS = "PROVIDER_GUT_IMMO_BUY_IDS";
-
-        public static string PROVIDER_GUT_IMMO_CATEGORY_IDS = "PROVIDER_GUT_IMMO_CATEGORY_IDS";
-    }
-}
--- src/PropertyBot.Provider.RjImmobau/EnvironmentConstants.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PropertyBot.Provider.RjImmobau
-{
-    internal static class EnvironmentConstants
-    {
-        public static string PROVIDER_RJIMMO_IMMO_BUY_IDS = "PROVIDER_RJIMMO_IMMO_BUY_IDS";
-
-        public static string PROVIDER_RJIMMO_IMMO_CATEGORY_IDS = "PROVIDER_RJIMMO_IMMO_CATEGORY_IDS";
-    }
-}
