a8e03b1e2c392638ae01d3b58e90fefe6c18c857
Niklas <niklas@nokxs.de>
2020-11-13 10:24:14 +0100

    Add logger in common

---
+++ src/PropertyBot.Common/Logging/Logger.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using Microsoft.Extensions.Logging;
+
+namespace PropertyBot.Common.Logging
+{
+    public class Logger<T> : ILogger
+    {
+        private readonly ILogger<T> _logger;
+
+        public Logger(ILogger<T> logger)
+        {
+            _logger = logger;
+        }
+
+        public void LogDebug(string message, params object[] args)
+        {
+            _logger.LogDebug(message, args);
+        }
+
+        public void LogInfo(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogWarning(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogCritical(string message, params object[] args)
+        {
+            _logger.LogInformation(message, args);
+        }
+
+        public void LogCritical(Exception exception, string message, params object[] args)
+        {
+            _logger.LogInformation(exception, message, args);
+        }
+    }
+}
