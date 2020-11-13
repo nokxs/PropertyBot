a5a1cc625453e7947bb501b19d35d9b58c452126
Niklas <niklas@nokxs.de>
2020-11-13 10:30:21 +0100

    Add Ioc Container abstraction

---
+++ src/PropertyBot.Common/Ioc/IIocContainer.cs
@@ -0,0 +1,8 @@
+﻿namespace PropertyBot.Common.Ioc
+{
+    public interface IIocContainer
+    {
+        void AddSingleton<TService>() where TService : class;
+        void AddSingleton<TService>(TService service) where TService : class;
+    }
+}
---
+++ src/PropertyBot.Common/Ioc/IocContainer.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PropertyBot.Common.Ioc
+{
+    public class IocContainer : IIocContainer
+    {
+        private readonly IServiceCollection _serviceCollection;
+
+        public IocContainer(IServiceCollection serviceCollection)
+        {
+            _serviceCollection = serviceCollection;
+        }
+
+        public void AddSingleton<TService>() where TService : class
+        {
+            _serviceCollection.AddSingleton<TService>();
+        }
+
+        public void AddSingleton<TService>(TService service) where TService : class
+        {
+            _serviceCollection.AddSingleton(service);
+        }
+    }
+}
