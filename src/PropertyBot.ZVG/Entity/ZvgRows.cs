095a01da05f8f74ca4ea113400b6a8930188e28c
Oliver Kabierschke <code@oliver-kabierschke.de>
2021-03-14 16:17:42 +0100

    Ignore ZVG exceptions

--- src/PropertyBot.ZVG/Entity/ZvgRows.cs
+++ src/PropertyBot.ZVG/Entity/ZvgRows.cs
@@ -1,4 +1,5 @@
 ﻿using System.Collections.Generic;
+using System.Linq;
 using System.Text.Json.Serialization;
 
 namespace PropertyBot.Provider.ZVG.Entity
@@ -6,6 +7,6 @@ namespace PropertyBot.Provider.ZVG.Entity
     internal class ZvgRows
     {
         [JsonPropertyName("rows")]
-        public IEnumerable<ZvgObject> Rows { get; set; }
+        public IEnumerable<ZvgObject> Rows { get; set; } = Enumerable.Empty<ZvgObject>();
     }
 }
--- src/PropertyBot.ZVG/WebClient/ZvgWebClient.cs
+++ src/PropertyBot.ZVG/WebClient/ZvgWebClient.cs
@@ -1,7 +1,9 @@
 ﻿using System;
+using System.Linq;
 using System.Net.Http;
 using System.Text.Json;
 using System.Threading.Tasks;
+using PropertyBot.Common.Logging;
 using PropertyBot.Provider.ZVG.Entity;
 
 namespace PropertyBot.Provider.ZVG.WebClient
@@ -9,16 +11,24 @@ namespace PropertyBot.Provider.ZVG.WebClient
     internal class ZvgWebClient : IZvgWebClient
     {
         private readonly HttpClient _client;
+        private readonly ILogger<ZvgClient> _logger;
 
-        public ZvgWebClient()
+        public ZvgWebClient(
+            ILogger<ZvgClient> logger)
         {
             _client = new HttpClient { BaseAddress = new Uri("https://www.zvg.com") };
+            _logger = logger;
         }
 
         public async Task<ZvgRows> GetObjects(ZvgWebClientOptions options)
         {
-            var streamTask = _client.GetStreamAsync($"appl/termine.prg?act=getGridData&vt=&id_b={options.StateId}&ids={options.ObjectKindIds}&ido={options.CourtIds}&sort=a");
-            return await JsonSerializer.DeserializeAsync<ZvgRows>(await streamTask);
+            try {
+                var streamTask = _client.GetStreamAsync($"appl/termine.prg?act=getGridData&vt=&id_b={options.StateId}&ids={options.ObjectKindIds}&ido={options.CourtIds}&sort=a");
+                return await JsonSerializer.DeserializeAsync<ZvgRows>(await streamTask);
+            } catch (Exception e) {
+                _logger.LogCritical(e, "Could not parse answer from ZVG");
+                return new ZvgRows();
+            }
         }
     }
 }
