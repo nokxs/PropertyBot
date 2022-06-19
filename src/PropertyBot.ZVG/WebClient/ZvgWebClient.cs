d699cb3e40572bb240581d65963c4ff2c563fa2e
Oliver Kabierschke <code@oliver-kabierschke.de>
2022-06-19 13:01:15 +0200

    ZVG requires cookies

--- src/PropertyBot.ZVG/WebClient/ZvgWebClient.cs
+++ src/PropertyBot.ZVG/WebClient/ZvgWebClient.cs
@@ -16,18 +16,35 @@ namespace PropertyBot.Provider.ZVG.WebClient
         public ZvgWebClient(
             ILogger<ZvgClient> logger)
         {
-            _client = new HttpClient { BaseAddress = new Uri("https://www.zvg.com") };
+            var handler = new HttpClientHandler();
+            handler.CookieContainer = new System.Net.CookieContainer();
+            handler.UseCookies = true;
+            handler.UseDefaultCredentials = true;
+            _client = new HttpClient(handler) { BaseAddress = new Uri("https://www.zvg.com") };
             _logger = logger;
         }
 
         public async Task<ZvgRows> GetObjects(ZvgWebClientOptions options)
         {
+            await SetCookie();
+            var url = $"appl/termine.prg?act=getGridData&vt=&id_b={options.StateId}&ids={options.ObjectKindIds}&ido={options.CourtIds}&sort=a";
             try {
-                var streamTask = _client.GetStreamAsync($"appl/termine.prg?act=getGridData&vt=&id_b={options.StateId}&ids={options.ObjectKindIds}&ido={options.CourtIds}&sort=a");
+                var streamTask = _client.GetStreamAsync(url);
                 return await JsonSerializer.DeserializeAsync<ZvgRows>(await streamTask);
             } catch (Exception e) {
-                _logger.LogCritical(e, "Could not parse answer from ZVG");
+                _logger.LogCritical(e, $"Could not parse answer from ZVG ({_client.BaseAddress}{url})");
                 return new ZvgRows();
+            }
+        }
+
+        private async Task SetCookie()
+        {
+            var url = $"appl/cookie.prg";
+            try {
+                await _client.GetStreamAsync(url);
+            } catch (Exception e) {
+                _logger.LogCritical(e, $"Could not set ZVG Cookie from URL {url}");
+                throw e;
             }
         }
     }
